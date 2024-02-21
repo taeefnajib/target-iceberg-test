c3335e4683dc76b558188be79d0aef00031426d9
taeefnajib <taeefnajib@gmail.com>
2024-02-21 06:39:38 +0600

    pp

--- target_iceberg/iceberg.py
+++ target_iceberg/iceberg.py
@@ -5,14 +5,9 @@ from pyiceberg.schema import Schema as PyicebergSchema
 from pyiceberg.io.pyarrow import pyarrow_to_schema
 
 
-def singer_to_pyarrow_schema(self, singer_schema: dict) -> PyarrowSchema:
+def singer_to_pyarrow_schema(singer_schema: dict) -> PyarrowSchema:
     """Convert singer tap json schema to pyarrow schema."""
-    
     def process_anyof_schema(anyOf: List) -> Tuple[List, Union[str, None]]:
-        """This function takes in original array of anyOf's schema detected
-        and reduces it to the detected schema, based on rules, right now
-        just detects whether it is string or not.
-        """
         types, formats = [], []
         for val in anyOf:
             typ = val.get("type")
@@ -32,26 +27,26 @@ def singer_to_pyarrow_schema(self, singer_schema: dict) -> PyarrowSchema:
         return ret_type, formats[0] if formats else None
 
     def get_pyarrow_schema_from_array(items: dict, level: int = 0):
-        type = items.get("type")
+        typ = items.get("type")
         any_of_types = items.get("anyOf")
 
         if any_of_types:
             self.logger.info("array with anyof type schema detected.")
-            type, _ = process_anyof_schema(anyOf=any_of_types)
+            typ, _ = process_anyof_schema(anyOf=any_of_types)
 
-        if "string" in type:
+        if "string" in typ:
             return pa.string()
-        elif "integer" in type:
+        elif "integer" in typ:
             return pa.int64()
-        elif "number" in type:
+        elif "number" in typ:
             return pa.float64()
-        elif "boolean" in type:
+        elif "boolean" in typ:
             return pa.bool_()
-        elif "array" in type:
+        elif "array" in typ:
             return pa.list_(
                 get_pyarrow_schema_from_array(items=items.get("items"), level=level)
             )
-        elif "object" in type:
+        elif "object" in typ:
             return pa.struct(
                 get_pyarrow_schema_from_object(
                     properties=items.get("properties"), level=level + 1
@@ -60,34 +55,30 @@ def singer_to_pyarrow_schema(self, singer_schema: dict) -> PyarrowSchema:
         else:
             return pa.null()
 
-    def get_pyarrow_schema_from_object(properties: dict, level: int = 0, field_id: int = 1):
-        """
-        Returns schema for an object.
-        """
+    def get_pyarrow_schema_from_object(properties: dict, level: int = 0):
         fields = []
+        field_id = 0
         for key, val in properties.items():
-            field_metadata = {"PARQUET:field_id": f"{field_id}"}
             field_id += 1
+            field_metadata = {"PARQUET:field_id": f"{field_id}"}
 
             if "type" in val.keys():
-                type = val["type"]
+                typ = val["type"]
                 format = val.get("format")
             elif "anyOf" in val.keys():
-                type, format = process_anyof_schema(val["anyOf"])
+                typ, format = process_anyof_schema(val["anyOf"])
             else:
-                self.logger.warning("type information not given")
-                type = ["string", "null"]
+                self.logger.info("type information not given")
+                typ = ["string", "null"]
 
-            if "integer" in type:
+            if "integer" in typ:
                 fields.append(pa.field(key, pa.int64(), metadata=field_metadata))
-            elif "number" in type:
+            elif "number" in typ:
                 fields.append(pa.field(key, pa.float64(), metadata=field_metadata))
-            elif "boolean" in type:
+            elif "boolean" in typ:
                 fields.append(pa.field(key, pa.bool_(), metadata=field_metadata))
-            elif "string" in type:
+            elif "string" in typ:
                 if format and level == 0:
-                    # this is done to handle explicit datetime conversion
-                    # which happens only at level 1 of a record
                     if format == "date":
                         fields.append(
                             pa.field(key, pa.date64(), metadata=field_metadata)
@@ -106,39 +97,53 @@ def singer_to_pyarrow_schema(self, singer_schema: dict) -> PyarrowSchema:
                         )
                 else:
                     fields.append(pa.field(key, pa.string(), metadata=field_metadata))
-            elif "array" in type:
+            elif "array" in typ:
                 items = val.get("items")
                 if items:
-                    item_type, field_id = get_pyarrow_schema_from_array(
-                        items=items, level=level, field_id=field_id
-                    )
+                    item_type = get_pyarrow_schema_from_array(items=items, level=level)
+                    if item_type == pa.null():
+                        self.logger.info(
+                            f"""key: {key} is defined as list of null, while this would be
+                                correct for a list of all null, it is better to define
+                                exact item types for the list, if not null."""
+                        )
                     fields.append(
                         pa.field(key, pa.list_(item_type), metadata=field_metadata)
                     )
                 else:
+                    self.logger.info(
+                        f"""key: {key} is defined as a list of null, while this would be
+                            correct for a list of all null, it is better to define
+                            exact item types for the list, if not null."""
+                    )
                     fields.append(
                         pa.field(key, pa.list_(pa.null()), metadata=field_metadata)
                     )
-            elif "object" in type:
+            elif "object" in typ:
                 prop = val.get("properties")
-                # Recursively handle nested properties
-                inner_fields, field_id = get_pyarrow_schema_from_object(
-                    properties=prop, level=level + 1, field_id=field_id
+                inner_fields = get_pyarrow_schema_from_object(
+                    properties=prop, level=level + 1
                 )
+                if not inner_fields:
+                    self.logger.info(
+                        f"""key: {key} has no fields defined, this may cause
+                            saving parquet failure as parquet doesn't support
+                            empty/null complex types [array, structs] """
+                    )
                 fields.append(
                     pa.field(key, pa.struct(inner_fields), metadata=field_metadata)
                 )
 
-        return fields, field_id
+        return fields
 
     properties = singer_schema.get("properties")
-    pyarrow_schema_fields, _ = get_pyarrow_schema_from_object(properties=properties)
+    pyarrow_schema = pa.schema(get_pyarrow_schema_from_object(properties=properties))
 
-    return pa.schema(pyarrow_schema_fields)
+    return pyarrow_schema
 
 
-def singer_to_pyiceberg_schema(self, singer_schema: dict) -> PyicebergSchema:
+def singer_to_pyiceberg_schema(singer_schema: dict) -> PyicebergSchema:
     """Convert singer tap json schema to pyiceberg schema via pyarrow schema."""
-    pyarrow_schema = singer_to_pyarrow_schema(self, singer_schema)
+    pyarrow_schema = singer_to_pyarrow_schema(singer_schema)
     pyiceberg_schema = pyarrow_to_schema(pyarrow_schema)
     return pyiceberg_schema
