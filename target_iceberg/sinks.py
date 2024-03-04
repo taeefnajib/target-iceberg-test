eca2ff31710d09ed07d5a62719cdd0399aef363e
taeefnajib <taeefnajib@gmail.com>
2024-03-04 21:34:56 +0600

    time is added

--- target_iceberg/sinks.py
+++ target_iceberg/sinks.py
@@ -47,52 +47,34 @@ class IcebergSink(BatchSink):
         start_time = time.time()
         self.logger.info("<<<<<Batch Started Processing>>>>>")
 
-        # Get the first few items of the context for debugging
-        n_items = dict(islice(context.items(), 3))
-        self.logger.warning(str(n_items))
-
-        # Extract records from the context
-        records = context.get("records", [])
-
-        # Define fields to drop from the schema
-        fields_to_drop = ["_sdc_deleted_at", "_sdc_table_version", "_sdc_extracted_at", "_sdc_received_at", "_sdc_batched_at", "_sdc_sequence", "_sdc_sync_started_at"]
-
-        # Drop unnecessary fields from the schema
-        singer_schema_narrow = {
-            "type": "object",
-            "properties": {
-                key: value
-                for key, value in self.schema["properties"].items()
-                if key not in fields_to_drop
-            },
-        }
-
-        # Convert records to a pyarrow table
-        df = pa.Table.from_pydict({"data": records})
-
-        # Create a PyArrow FileSystem object for S3
-        fs = fs.S3FileSystem()
+        # Create pyarrow df
+        fields_to_drop = ["_sdc_deleted_at", "_sdc_table_version"]
+        df = pa.Table.from_pylist(context["records"])
+        df_narrow = df.drop_columns(fields_to_drop)
 
         # Load the Iceberg catalog
+        # IMPORTANT: Make sure pyiceberg catalog env variables are set in the host machine - i.e. PYICEBERG_CATALOG__DEFAULT__URI, etc
+        #   - For more details, see: https://py.iceberg.apache.org/configuration/)
         region = fs.resolve_s3_region(self.config.get("s3_bucket"))
         catalog_name = self.config.get("iceberg_catalog_name")
         catalog = load_catalog(
             catalog_name,
-            uri=self.config.get("iceberg_rest_uri"),
-            s3_endpoint=os.environ.get(
-                "PYICEBERG_CATALOG__ICEBERGCATALOG__S3__ENDPOINT"
-            ),
-            py_io_impl="pyiceberg.io.pyarrow.PyArrowFileIO",
-            s3_region=region,
-            s3_access_key_id=os.environ.get(
-                "PYICEBERG_CATALOG__ICEBERGCATALOG__S3__ACCESS_KEY_ID"
-            ),
-            s3_secret_access_key=os.environ.get(
-                "PYICEBERG_CATALOG__ICEBERGCATALOG__S3__SECRET_ACCESS_KEY"
-            ),
+            **{
+                "uri": self.config.get("iceberg_rest_uri"),
+                "s3.endpoint": os.environ.get(
+                    "PYICEBERG_CATALOG__ICEBERGCATALOG__S3__ENDPOINT"
+                ),
+                "py-io-impl": "pyiceberg.io.pyarrow.PyArrowFileIO",
+                "s3.region": region,
+                "s3.access-key-id": os.environ.get(
+                    "PYICEBERG_CATALOG__ICEBERGCATALOG__S3__ACCESS_KEY_ID"
+                ),
+                "s3.secret-access-key": os.environ.get(
+                    "PYICEBERG_CATALOG__ICEBERGCATALOG__S3__SECRET_ACCESS_KEY"
+                ),
+            },
         )
 
-        # List namespaces in the catalog
         nss = catalog.list_namespaces()
         self.logger.info(f"Namespaces: {nss}")
 
@@ -101,23 +83,30 @@ class IcebergSink(BatchSink):
         try:
             catalog.create_namespace(ns_name)
             self.logger.info(f"Namespace '{ns_name}' created")
-        except (NamespaceAlreadyExistsError, NoSuchNamespaceError):
+        except (NamespaceAlreadyExistsError, NoSuchNamespaceError): 
+            # NoSuchNamespaceError is also raised for some reason (probably a bug - but needs to be handled anyway)
             self.logger.info(f"Namespace '{ns_name}' already exists")
 
         # Create a table if it doesn't exist
         table_name = self.stream_name
         table_id = f"{ns_name}.{table_name}"
+        singer_schema = self.schema
+        singer_schema_narrow = singer_schema
+        singer_schema_narrow["properties"] = {x: singer_schema["properties"][x] for x in singer_schema["properties"] if x not in fields_to_drop}
+
         try:
             table = catalog.load_table(table_id)
             self.logger.info(f"Table '{table_id}' loaded")
+
+            # TODO: Handle schema evolution - compare existing table schema with singer schema (converted to pyiceberg schema)
         except NoSuchTableError as e:
             # Table doesn't exist, so create it
-            table_schema = singer_to_pyiceberg_schema(singer_schema_narrow)
+            table_schema = singer_to_pyiceberg_schema(self, singer_schema)
             table = catalog.create_table(table_id, schema=table_schema)
             self.logger.info(f"Table '{table_id}' created")
 
         # Add data to the table
-        table.append(df)
+        table.append(df_narrow)
 
         end_time = time.time()  # Record the end time
         
